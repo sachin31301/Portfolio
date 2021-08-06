983638abbee248acbf42fd298821989fa9da0da7
Sachin <sachin31301@gmail.com>
2021-08-06 17:39:41 +0530

    resume updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A Software Engineer who likes to work on Softwares and scalable  Application which uses modern technologies like machine learning  to create impact.",
   resumeLink:
-    "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
+    "https://drive.google.com/file/d/1kepy5kkB7R_3BuILO9luTIq6bMONajcg/view?usp=sharing",
   portfolio_repository: "https://github.com/Sachin1919/masterPortfolio",
 };
 
